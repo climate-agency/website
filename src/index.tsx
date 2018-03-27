11f7892a45c75c41b6a70a183342b58af4fdbf8f
Evgenij Beloded <belodedevgenij@gmail.com>
2018-03-27 11:37:26 +0700

    Include Google Analytis

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { hydrate, render } from 'react-dom';
 import Website from './Website';
-import * as ReactGA from 'react-ga';
+// import * as ReactGA from 'react-ga';
 import { BrowserRouter } from 'react-router-dom';
 
-if (process.env.NODE_ENV === 'production') {
-  ReactGA.initialize('UA-108008469-1');
-}
+// if (process.env.NODE_ENV === 'production') {
+//   ReactGA.initialize('UA-108008469-1');
+// }
 
 const rootElement = document.getElementById('root') as HTMLElement;
 
